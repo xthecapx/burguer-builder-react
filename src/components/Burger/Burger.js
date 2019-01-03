290305384018e08073ee288082eaf5e7efef2a69
Cristian Marquez <xthecapx@gmail.com>
2019-01-03 17:26:48 -0500

    Created dynamic elements on burger builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,16 @@ import React from 'react';
 import classes from './Burger.module.scss';
 import BurgerIngrient from './BurgerIngredient/BurgerIngredient';
 
-function Burger() {
+function Burger(props) {
+  const ingredients = props.ingredients;
+  const ingridientsElements = Object.keys(ingredients).map(key => {
+    return Array.from(Array(ingredients[key]), (_, index) => <BurgerIngrient key={key + index} type={key} />);
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngrient type='bread-top' />
-      <BurgerIngrient type='cheese' />
-      <BurgerIngrient type='meat' />
+      {ingridientsElements}
       <BurgerIngrient type='bread-bottom' />
     </div>
   );
