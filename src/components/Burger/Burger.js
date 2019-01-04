ac2f3dbb050ff2f7bfaa5ef9e7b5da3de3ad4d68
Cristian Marquez <xthecapx@gmail.com>
2019-01-04 10:34:49 -0500

    Added case when to start adding ingridients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,9 +4,14 @@ import BurgerIngrient from './BurgerIngredient/BurgerIngredient';
 
 function Burger(props) {
   const ingredients = props.ingredients;
-  const ingridientsElements = Object.keys(ingredients).map(key => {
+  let ingridientsElements = Object.keys(ingredients).map(key => {
     return Array.from(Array(ingredients[key]), (_, index) => <BurgerIngrient key={key + index} type={key} />);
-  });
+  }).reduce((acc, element) => acc.concat(element), []);
+  console.log(ingridientsElements);
+
+  if (ingridientsElements.length === 0) {
+    ingridientsElements = <p>Please start adding ingredients!!</p>
+  }
 
   return (
     <div className={classes.Burger}>
