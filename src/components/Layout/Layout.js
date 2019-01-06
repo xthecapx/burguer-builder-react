a61de9ffc8c707eefd13c4f4e2ac0f56b369dafc
Cristian Marquez <xthecapx@gmail.com>
2019-01-06 10:33:36 -0500

    Added toobar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Layout.module.scss';
+import ToolBar from '../Navigation/ToolBar/ToolBar';
 
 function Layout(props) {
   return (
     <>
-      <div>Toolbar, SideDrawer, Backgrop</div>
+      <ToolBar />
       <main className={classes.Content}>{props.children}</main>
     </>
   );
---
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './ToolBar.module.scss';
+
+function ToolBar(props) {
+  return (
+    <header className={classes.ToolBar}>
+      <div>Menu</div>
+      <div>LOGO</div>
+      <nav>...</nav>
+    </header>
+  );
+}
+
+export default ToolBar;
