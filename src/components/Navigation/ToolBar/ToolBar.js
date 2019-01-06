15507cca7d851354b88cc80ce4fdcb287edadd27
Cristian Marquez <xthecapx@gmail.com>
2019-01-06 10:45:03 -0500

    Added logo to the toolbar

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './ToolBar.module.scss';
+import Logo from '../../Logo/Logo';
 
 function ToolBar(props) {
   return (
     <header className={classes.ToolBar}>
       <div>Menu</div>
-      <div>LOGO</div>
+      <Logo />
       <nav>...</nav>
     </header>
   );
