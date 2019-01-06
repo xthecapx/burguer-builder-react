1fcbf06bf4726d5230b45913fd75d827a26fb547
Cristian Marquez <xthecapx@gmail.com>
2019-01-06 12:10:06 -0500

    Close status of sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.scss';
 import ToolBar from '../Navigation/ToolBar/ToolBar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-function Layout(props) {
-  return (
-    <>
-      <ToolBar />
-      <SideDrawer />
-      <main className={classes.Content}>{props.children}</main>
-    </>
-  );
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <>
+        <ToolBar />
+        <SideDrawer show={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
 }
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.scss';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 function SideDrawer(props) {
+  let navigationClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.show) {
+    navigationClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <>
+      <Backdrop show={props.show} clicked={props.closed} />
+      <div className={navigationClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </>
   );
 }
 
