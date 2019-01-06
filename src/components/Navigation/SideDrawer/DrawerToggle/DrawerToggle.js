d2af8bb86bde0b9da05e3649b804ad60b3dcc8f3
Cristian Marquez <xthecapx@gmail.com>
2019-01-06 12:27:37 -0500

    Added hamburger to drawerToggle

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import classes from './DrawerToggle.module.scss';
 
 function DrawerToggle(props) {
-  return <div onClick={props.menuClicked}>Menu</div>;
+  return (
+    <div className={classes.DrawerToggle} onClick={props.menuClicked}>
+      <div />
+      <div />
+      <div />
+    </div>
+  );
 }
 
 export default DrawerToggle;
