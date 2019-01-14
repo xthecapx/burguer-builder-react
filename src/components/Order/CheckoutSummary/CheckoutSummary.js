7a1bcc18757b181ea3798192dddbb63775e09222
Cristian Marquez <xthecapx@gmail.com>
2019-01-14 16:56:12 -0500

    cancel and continue actions on checkout summary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -11,8 +11,12 @@ function CheckoutSummary(props) {
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType='Danger'>CANCEL</Button>
-      <Button btnType='Success'>CONTINUE</Button>
+      <Button btnType='Danger' clicked={props.onCheckoutCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType='Success' clicked={props.onCheckoutContinued}>
+        CONTINUE
+      </Button>
     </div>
   );
 }
