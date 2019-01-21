b2cb7bb944694dd35d647f1856e5b7b63e797bc6
Cristian Marquez <xthecapx@gmail.com>
2019-01-21 13:21:59 -0500

    Created actions and reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as types from './actions';
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
